14c5a5e08fc4695a7e658f944907be02dc7c694b
Morton Fox <github@qslw.com>
2017-06-21 17:25:19 -0400

    Clean up and comment

--- reportzee2.rb
+++ reportzee2.rb
@@ -13,6 +13,7 @@ class MunzeeAPI
   class Callback < WEBrick::HTTPServlet::AbstractServlet
     @auth_code = nil
 
+    # Handler will extract the auth code from the callback request.
     def do_GET request, response
       if request.query['code']
         self.class.instance_variable_set(:@auth_code, request.query['code']) unless self.class.instance_variable_get(:@auth_code)
@@ -44,6 +45,9 @@ class MunzeeAPI
 
     load_token unless params[:force_login]
 
+    # If the token file cannot be read or the access token has expired or the
+    # force_login parameter is true, authenticate with Munzee to get a new
+    # access token.
     login if @token.nil? || @token.expired?
   end
 
@@ -60,6 +64,7 @@ class MunzeeAPI
     File.open(File.expand_path(TOKEN_FILE)) { |io|
       token_hash = JSON.parse(io.read)
       @token = OAuth2::AccessToken.from_hash(@client, token_hash)
+      @token.options[:header_format] = '%s'
     }
   rescue
     nil
@@ -68,34 +73,34 @@ class MunzeeAPI
   def login
     url = @client.auth_code.authorize_url(redirect_uri: REDIRECT_URL, scope: 'read')
 
+    # Run a WEBrick server to catch the callback after the user authorizes the
+    # app.
     log = WEBrick::Log.new($stdout, WEBrick::Log::ERROR)
     server = WEBrick::HTTPServer.new(Port: 8558, Logger: log, AccessLog: [])
     server.mount '/oauth2/callback', Callback
 
-    Thread.new {
-      server.start
-    }
+    Thread.new { server.start }
 
+    # Open the authorization URL and wait for the callback.
     Launchy.open(url)
 
     auth_code = Callback.wait_auth_code
 
     server.shutdown
-    # puts "code = #{auth_code}"
 
     token1 = @client.auth_code.get_token(auth_code, :redirect_uri => REDIRECT_URL)
 
+    # Munzee returns the token two levels deep in the JSON response. Extract
+    # the token and recreate our AccessToken.
     token_hash = token1.params['data']['token']
-    # p token_hash
 
     @token = OAuth2::AccessToken.from_hash(@client, token_hash)
     @token.options[:header_format] = '%s'
 
+    # Save the token to the token file.
     File.open(File.expand_path(TOKEN_FILE), 'w') { |io|
       io.write(@token.to_hash.to_json)
     }
-
-    # puts "Bearer token: #{@token.token}"
   end
 
   def post path, params
