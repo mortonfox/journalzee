e9d1a85396aa1ebc2e833d86b4d813282ef75fa9
Morton Fox <github@qslw.com>
2017-06-21 14:40:12 -0400

    Wrap Munzee API code in its own class

--- reportzee2.rb
+++ reportzee2.rb
@@ -1,68 +1,88 @@
 require 'oauth2'
 require 'webrick'
 
-CLIENT_ID = '********'
-CLIENT_SECRET = '********'
-REDIRECT_URL = 'http://localhost:8558/oauth2/callback'
+class MunzeeAPI
+  CLIENT_ID = '********'
+  CLIENT_SECRET = '********'
+  REDIRECT_URL = 'http://localhost:8558/oauth2/callback'
 
-$code = nil
+  class HTTPError < StandardError
+  end
 
-class Callback < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET request, response
-    if request.query['code']
-      $code = request.query['code'] unless $code
+  class Callback < WEBrick::HTTPServlet::AbstractServlet
+    @auth_code = nil
 
-      response.status = 200
-      response.content_type = 'text/plain'
-      response.body = 'Okay'
+    def do_GET request, response
+      if request.query['code']
+        self.class.instance_variable_set(:@auth_code, request.query['code']) unless self.class.instance_variable_get(:@auth_code)
+        response.body = 'Okay'
+        raise WEBrick::HTTPStatus::OK
+      else
+        response.body = 'Missing code parameter'
+        raise WEBrick::HTTPStatus::BadRequest
+      end
+    end
+
+    def self.wait_auth_code
+      while not @auth_code
+        sleep 1
+      end
+      @auth_code
+    end
+  end
+
+  def initialize
+    @token = nil
+  end
+
+  def login
+    client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET,
+                                site: 'https://api.munzee.com',
+                                authorize_url: '/oauth',
+                                token_url: '/oauth/login',
+                                raise_errors: false)
+    url = client.auth_code.authorize_url(redirect_uri: REDIRECT_URL, scope: 'read')
+
+    server = WEBrick::HTTPServer.new(Port: 8558)
+    server.mount '/oauth2/callback', Callback
+
+    Thread.new {
+      server.start
+    }
+
+    system("open '#{url}'")
+
+    auth_code = Callback.wait_auth_code
+
+    server.shutdown
+    puts "code = #{auth_code}"
+
+    token1 = client.auth_code.get_token(auth_code, :redirect_uri => REDIRECT_URL)
+
+    token_hash = token1.params['data']['token']
+    p token_hash
+
+    @token = OAuth2::AccessToken.from_hash(client, token_hash)
+    @token.options[:header_format] = '%s'
+
+    puts "Bearer token: #{@token.token}"
+  end
+
+  def post path, params
+    res = @token.post(path, body: "data=#{params.to_json}")
+    if res.status == 200
+      res.parsed
     else
-      response.status = 400
-      response.content_type = 'text/plain'
-      response.body = 'Missing code parameter'
+      raise HTTPError, "#{res.status} #{res.body}"
     end
   end
 end
 
-client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET,
-                            site: 'https://api.munzee.com',
-                            authorize_url: '/oauth',
-                            token_url: '/oauth/login',
-                            raise_errors: false)
-url = client.auth_code.authorize_url(redirect_uri: REDIRECT_URL, scope: 'read')
+munz = MunzeeAPI.new
 
-server = WEBrick::HTTPServer.new(Port: 8558)
-server.mount '/oauth2/callback', Callback
+munz.login
 
-Thread.new {
-  server.start
-}
-
-system("open '#{url}'")
-
-while not $code
-  sleep 1
-end
-
-server.shutdown
-puts "code = #{$code}"
-
-token1 = client.auth_code.get_token($code, :redirect_uri => REDIRECT_URL)
-
-token_hash = token1.params['data']['token']
-p token_hash
-
-token = OAuth2::AccessToken.from_hash(client, token_hash)
-token.options[:header_format] = '%s'
-
-puts "Bearer token: #{token.token}"
-
-res = token.post('/user', body: "data=#{{username: 'mortonfox'}.to_json}")
-
-case res.status
-when 200
-  p res.parsed
-else
-  warn "#{res.status} #{res.body}"
-end
+result = munz.post('/user', username: 'mortonfox')
+p result
 
 __END__
